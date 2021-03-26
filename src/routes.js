3085fb7650a64ab50f25104b9e83097aa0438ca5
Salomao Souza <souza95salomao@gmail.com>
2021-03-26 12:09:37 -0400

    [UPDATE] Paths

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const routes = express.Router()
 
-const basePath = __dirname+'/pages'
+const views = __dirname + '/views/'
 
-routes.get('/', (req, res)=>res.sendFile(basePath+'/index.html'))
-routes.get('/job', (req, res)=>res.sendFile(basePath+'/job.html'))
-routes.get('/job/edit', (req, res)=>res.sendFile(basePath+'/job-edit.html'))
-routes.get('/profile', (req, res)=>res.sendFile(basePath+'/profile.html'))
+routes.get('/', (req, res)=>res.render(views + 'index'))
+routes.get('/job', (req, res)=>res.render(views + 'job'))
+routes.get('/job/edit', (req, res)=>res.render(views + 'job-edit'))
+routes.get('/profile', (req, res)=>res.render(views + 'profile'))
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const server = express()
 const routes = require('./routes')
 
-//hability to static files
+server.set('view engine', 'ejs')
+
 server.use(express.static('public'))
+
 server.use(routes)
 
 server.listen(3000, ()=>console.log('server is running..'))
