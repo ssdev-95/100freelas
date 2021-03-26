3dfb71b5e55d4bed4d58621d6a10140fe00aec59
Salomao Souza <souza95salomao@gmail.com>
2021-03-26 12:46:36 -0400

    [UPDATE] Implementing EJS support

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,17 @@ const routes = express.Router()
 
 const views = __dirname + '/views/'
 
+const profile = {
+    name: 'Salomao',
+    avatar: 'https://avatars.githubusercontent.com/u/58428963?v=4',
+    monthlyBudget: 2650.99,
+    daysPerWeek: 5,
+    hoursPerDay: 6,
+    vacationPerYear: 12
+}
 routes.get('/', (req, res)=>res.render(views + 'index'))
 routes.get('/job', (req, res)=>res.render(views + 'job'))
 routes.get('/job/edit', (req, res)=>res.render(views + 'job-edit'))
-routes.get('/profile', (req, res)=>res.render(views + 'profile'))
+routes.get('/profile', (req, res)=>res.render(views + 'profile', { profile }))
 
 module.exports = routes
