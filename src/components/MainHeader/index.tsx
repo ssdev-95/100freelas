6aa9e905969682d7b53a63cec48e3cd16e554e5b
Salomao Souza <souza95salomao@gmail.com>
2021-04-09 14:30:41 -0400

    [UPDATE] Navbar style

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,17 +1,24 @@
-import { Nav, Line, Div, Logo, Text } from './styles'
+import styles from '../../styles/components/HomeHead.module.css'
 
 export default function Header() {
     return (
-      <Nav>
-        <Div style={{width: '104px'}}>
-          <Logo src="images/logo.svg"/>
-          <div style={{display: 'flex'}}>
+      <nav className={styles.navbar}>
+        <div className={styles.topBar}>
+          <img className={styles.logo} src="images/logo.svg" alt="Logo" />
+          <div className={styles.freehours}>
             <img src="images/alert.svg" alt=""/>
-            <Text>Voce tem 2 horas livres no seu dia</Text>
+            <h2>Voce tem 2 horas livres no seu dia</h2>
           </div>
-        </Div>
-        <Line />
-       <Div style={{width: '176px'}}></Div>
-      </Nav>
+          <div className={styles.profileContainer}>
+            <div>
+              <h3>Salomao Souza</h3>
+              <h3><a href="#">Ver perfil</a></h3>
+            </div>
+            <img src="https://github.com/xSallus.png" alt=""/>
+          </div>
+        </div>
+        <hr className={styles.line} />
+       <div className={styles.bottomBar}></div>
+      </nav>
     )
   }
