4ee983d8f03a9e97f18615982bb60fa4dec8e931
Salomao Souza <souza95salomao@gmail.com>
2021-04-09 09:35:08 -0400

    [REfACTORING] Moving to NextJS

---
+++ src/components/JobHeader/index.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div>
+      <a href="/">
+        <img src="images/back.svg" alt="" />
+      </a>
+      <h1>Title</h1>
+    </div>
+  )
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider, theme, CSSReset } from '@chakra-ui/core';
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <ThemeProvider theme={theme}>
+            <CSSReset />
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
+}
+
+export default MyApp
