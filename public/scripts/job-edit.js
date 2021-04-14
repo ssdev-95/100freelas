7bc2113f56f3112a63d08de243811500fa23e7ab
Salomao Souza <souza95salomao@gmail.com>
2021-04-14 14:00:55 -0400

    [ADD] Enviroment variables

--- public/scripts/job-edit.js
+++
@@ -1,7 +0,0 @@
-import Modal from './modal.js';
-
-const modal = Modal({ animateClasses: ['animate-pop', 'back'] })
-
-document
-  .querySelector('.open-modal')
-  .addEventListener('click', modal.open)
--- src/db/config.tsx
+++ src/db/config.tsx
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBKPITDD7fTKivzrhU3VbWmzRG1AIa4Xi0",
-    authDomain: "jobs-calc-beta.firebaseapp.com",
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
     projectId: "jobs-calc-beta",
-    storageBucket: "jobs-calc-beta.appspot.com",
-    messagingSenderId: "497783524681",
-    appId: "1:497783524681:web:2508ceb2a5bb3be4769e98"
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
 }
  
 firebase.apps.length<1 && firebase.initializeApp(firebaseConfig)
